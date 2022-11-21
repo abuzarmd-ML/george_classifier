87c5c361e9b306df23fb9cef257bdaa1c73e0f15
Abuzar <abuzar@maxerience.com>
2022-11-21 19:25:14 +0530

    Script to classify images on local machine

--- inference_classification_local.sh
+++ inference_classification_local.sh
@@ -1,2 +1,2 @@
-python yolov7/detect.py --weights yolov7/runs/train/yolov7-custom2/weights/best.pt --conf 0.5 --img-size 640 --source ./test/53f3e0250f5db7f5a997755dc6dd56b6.jpg --no-trace
+python yolov7/detect.py --weights yolov7/runs/train/yolov7-custom2/weights/best.pt --conf 0.5 --img-size 640 --source ./test --no-trace
 #cp -r yolov7/runs/detect/exp/ ../classification_result/
